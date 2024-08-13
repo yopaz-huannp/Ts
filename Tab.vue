d1bfb8401bdacafe736ecf69a85d11e07c93548e
huanpn-yopaz <87886188+huanpn-yopaz@users.noreply.github.com>
2024-08-13 09:55:42 +0700

    Create Tab.vue

---
+++ Tab.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const tab = ref<string>('tab1');
+// define method
+const changeTab = (value: string) => {
+    tab.value = value
+}
+</script>
+<template>
+    
+    <div class=" flex py-3 flex-wrap">
+        <div class="detail_tour mr-2 sm:mr-7 p-2" @click="changeTab('tab1')">
+            Thông tin
+        </div>
+        <div class=" detail_tour mr-2 sm:mr-7 px-2 p-2" @click="changeTab('tab2')">
+            Tổng quan
+        </div>
+    </div>
+    <div class="content_tab hidden animate-overlay" :class="{ active: tab === 'tab1' }">
+        <div>tab 1</div>
+    </div>
+    <div class="content_tab hidden animate-overlay" :class="{ active: tab === 'tab2' }">
+        <div>tab 2</div>
+    </div>
+    <!-- end form edit name -->
+</template>
+<style scoped>
+.bg-statistic {
+    background: #F7F6F1;
+}
+
+.content_tab.active {
+    display: block;
+}
+</style>
